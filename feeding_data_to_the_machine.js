b6778b1577c7f4a7d11cd948aee9d92f46a64913
sriramdex <48167231+sriramdex@users.noreply.github.com>
2019-08-31 22:11:47 +0530

    Update feeding_data_to_the_machine.js

--- feeding_data_to_the_machine.js
+++ feeding_data_to_the_machine.js
@@ -1,30 +1,10 @@
-const classifier = knnClassifier.create();
 const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
 let net;
 
-async function setupWebcam() {
-  return new Promise((resolve, reject) => {
-    const navigatorAny = navigator;
-    navigator.getUserMedia = navigator.getUserMedia ||
-        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
-        navigatorAny.msGetUserMedia;
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia({video: true},
-        stream => {
-          webcamElement.srcObject = stream;
-          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
-        },
-        error => reject());
-    } else {
-      reject();
-    }
-  });
-}
 
-
-
- async function app() {
-  console.log('Loading mobilenet..');
+async function app() {
+console.log('Loading mobilenet..');
 
   // Load the model.
   net = await mobilenet.load();
@@ -33,7 +13,7 @@ async function setupWebcam() {
   await setupWebcam();
 
   // Reads an image from the webcam and associates it with a specific class
-  // classfeeding_data_to_the_machine.
+  // index.
   const addExample = classId => {
     // Get the intermediate activation of MobileNet 'conv_preds' and pass that
     // to the KNN classifier.
@@ -56,10 +36,10 @@ async function setupWebcam() {
       // Get the most likely class and confidences from the classifier module.
       const result = await classifier.predictClass(activation);
 
-      const classes = ['Sriram', 'Ritesh', 'Sriram and Ritesh', 'Sriram and Doll'];
+      const classes = ['Dad', 'Mom', 'Dad and Mom','object 4'];
       document.getElementById('console').innerText = `
-        prediction: ${classes[result.classfeeding_data_to_the_machine]}\n
-        probability: ${result.confidences[result.classfeeding_data_to_the_machine]}
+        prediction: ${classes[result.classIndex]}\n
+        probability: ${result.confidences[result.classIndex]}
       `;
     }
 
@@ -67,6 +47,23 @@ async function setupWebcam() {
   }
 }
 
-
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
 
 app();
